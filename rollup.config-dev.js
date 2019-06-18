0f9243e3b81c72fe4bf8a5fb253361d0c4b65bf8
Dario Vladović <d.vladimyr@gmail.com>
2019-06-18 07:25:01 +0200

    Fix ESLint error :bug:

--- rollup.config-dev.js
+++ rollup.config-dev.js
@@ -8,13 +8,13 @@ module.exports = baseConfig.map((config, index) => {
   config.plugins.push(plugin({
     open: true,
     filename: `stats.${index}.html`,
-    template: getTemplate(config)
+    template: getTemplateType(config)
   }));
   return config;
 });
 
-function getTemplate({ input }) {
+function getTemplateType({ input }) {
   const filename = path.basename(input, path.extname(input));
-  const [_, template] = filename.split("-");
-  return template;
+  const [, templateType] = filename.split("-");
+  return templateType;
 }
