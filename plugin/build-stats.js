a9ea171dbf22506c72fca1874952e2bc6e59b80e
Jacob Bowns <Jacob.Bowns@gmail.com>
2019-09-12 13:07:27 +0100

    add styleOverridePath option to inject user defined css styling (#45)

--- plugin/build-stats.js
+++ plugin/build-stats.js
@@ -5,17 +5,19 @@ const path = require("path");
 const pupa = require("pupa");
 const readFile = require("util").promisify(fs.readFile);
 
-module.exports = async function buildHtml(title, nodesData, graphType) {
-  const [template, script, style] = await Promise.all([
+module.exports = async function buildHtml(title, nodesData, graphType, styleOverridePath) {
+  const [template, script, style, styleOverride] = await Promise.all([
     readFile(path.join(__dirname, "stats.template"), "utf-8"),
     readFile(path.join(__dirname, "..", "lib", `main-${graphType}.js`), "utf8"),
-    readFile(path.join(__dirname, "..", "lib", `style-${graphType}.css`), "utf8")
+    readFile(path.join(__dirname, "..", "lib", `style-${graphType}.css`), "utf8"),
+    styleOverridePath ? readFile(styleOverridePath, "utf8") : Promise.resolve()
   ]);
 
   return pupa(template, {
     title,
     style,
     script,
+    styleOverride,
     nodesData: JSON.stringify(nodesData)
   });
 };
