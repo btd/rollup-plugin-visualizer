4ebfc1af92e3cd9324e5747e7ae70a5a46c3e069
Denis Bardadym <bardadymchik@gmail.com>
2020-04-03 13:55:34 +0200

    Copy env and options from files

--- bin/cli.js
+++ bin/cli.js
@@ -15,22 +15,22 @@ const argv = require("yargs")
   .option("filename", {
     describe: "Output file name",
     string: true,
-    default: "./stats.html"
+    default: "./stats.html",
   })
   .option("title", {
     describe: "Output file title",
     string: true,
-    default: "RollUp Visualizer"
+    default: "RollUp Visualizer",
   })
   .option("template", {
     describe: "Template type",
     string: true,
     choices: TEMPLATE,
-    default: "treemap"
+    default: "treemap",
   })
   .option("sourcemap", {
     describe: "Provided files is sourcemaps",
-    boolean: true
+    boolean: true,
   })
   .help().argv;
 
@@ -42,7 +42,7 @@ const runForPluginJson = async ({ title, template, filename }, files) => {
   }
 
   const fileContents = await Promise.all(
-    files.map(async file => {
+    files.map(async (file) => {
       const textContent = await fs.readFile(file, { encoding: "utf-8" });
       const jsonContent = JSON.parse(textContent);
       return [file, jsonContent];
@@ -51,7 +51,7 @@ const runForPluginJson = async ({ title, template, filename }, files) => {
 
   const tree = {
     name: "root",
-    children: []
+    children: [],
   };
   const nodes = Object.create(null);
   let links = [];
@@ -75,20 +75,27 @@ const runForPluginJson = async ({ title, template, filename }, files) => {
     links = links.concat(fileContent.links);
   }
 
-  const data = { version: JSON_VERSION, tree, links, nodes };
+  const data = {
+    version: JSON_VERSION,
+    tree,
+    links,
+    nodes,
+    env: fileContents[0].env,
+    options: fileContents[0].options,
+  };
 
   const fileContent = await buildStats({
     title,
     data,
     template,
-    chartParameters: {}
+    chartParameters: {},
   });
 
   await fs.mkdir(path.dirname(filename), { recursive: true });
   await fs.writeFile(filename, fileContent);
 };
 
-runForPluginJson(argv, listOfFiles).catch(err => {
+runForPluginJson(argv, listOfFiles).catch((err) => {
   warn(err.message);
   process.exit(1);
 });
