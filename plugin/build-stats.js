f178d3569b509b4ea9ec5ae925d143caef3e6aa1
Denis Bardadym <denis.bardadym@check24.de>
2020-11-03 19:47:46 +0100

    Add window resize event handler

--- plugin/build-stats.js
+++ plugin/build-stats.js
@@ -15,36 +15,40 @@ const buildHtmlTemplate = ({ title, script, nodesData, style }) =>
   `
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>${htmlEscape(title)}</title>
-    <style>
-    ${style}
-    </style>
-  </head>
-  <body>
-    <main></main>
-    <script>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>${htmlEscape(title)}</title>
+  <style>
+${style}
+  </style>
+</head>
+<body>
+  <main></main>
+  <script>
+  /*<!--*/
+${script}
+  /*-->*/
+  </script>
+  <script>
     /*<!--*/
-    ${script}
+    const data = ${nodesData};
+
+    const run = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      const chartNode = document.querySelector("main");
+      drawChart(chartNode, data, width, height);
+    };
+
+    window.addEventListener('resize', run);
+
+    document.addEventListener('DOMContentLoaded', run);
     /*-->*/
-    </script>
-    <script>
-      /*<!--*/
-      const data = ${nodesData};
-
-      document.addEventListener('DOMContentLoaded', () => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-
-        const chartNode = document.querySelector("main");
-        drawChart(chartNode, data, width, height);
-      });
-      /*-->*/
-    </script>
-  </body>
+  </script>
+</body>
 </html>
 
 `;
