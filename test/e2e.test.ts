61301879c0a45a8c59b891d5cee20d8559c4d112
Vladimir Klepov <vklepov@ozon.ru>
2023-05-17 12:43:04 +0300

    Add failing terser test

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,4 +1,5 @@
 import { OutputAsset, OutputOptions, rollup } from "rollup";
+import terser from '@rollup/plugin-terser';
 import { describe, it, expect } from "@jest/globals";
 import { ALL_TEMPLATE } from "./util";
 
@@ -8,12 +9,12 @@ Math.random = () => randomCall / 100;
 
 import { visualizer } from "../dist/plugin";
 
-describe("E2E", () => {
-  const input = {
-    input: "./test/e2e/input.js",
-    input2: "./test/e2e/input2.js",
-  };
+const input = {
+  input: "./test/e2e/input.js",
+  input2: "./test/e2e/input2.js",
+};
 
+describe("E2E", () => {
   it.each(ALL_TEMPLATE)("test - %j", async (template) => {
     const inputOptions = {
       external: ["jquery"],
@@ -46,11 +47,6 @@ describe("E2E", () => {
 });
 
 describe("E2E sourcemap", () => {
-  const input = {
-    input: "./test/e2e/input.js",
-    input2: "./test/e2e/input2.js",
-  };
-
   it.each(ALL_TEMPLATE)("test - %j", async (template) => {
     const inputOptions = {
       external: ["jquery"],
@@ -59,6 +55,41 @@ describe("E2E sourcemap", () => {
         visualizer({
           filename: `stats`,
           template,
+          exclude: [{ file: "**/node_modules/**" }],
+          emitFile: true,
+          sourcemap: true,
+          brotliSize: true,
+          gzipSize: true,
+        }),
+      ],
+    };
+    const outputOptions: OutputOptions = {
+      format: "es",
+      dir: "./temp/",
+      sourcemap: true,
+    };
+
+    const bundle = await rollup(inputOptions);
+
+    const result = await bundle.generate(outputOptions);
+
+    const generatedStats = result.output.find((file) => file.fileName === "stats");
+
+    expect(generatedStats).not.toBe(null);
+    expect((generatedStats as OutputAsset).source).toMatchSnapshot();
+  });
+});
+
+describe("E2E sourcemap + terser", () => {
+  it("test", async () => {
+    const inputOptions = {
+      external: ["jquery"],
+      input,
+      plugins: [
+        terser(),
+        visualizer({
+          filename: `stats`,
+          template: 'raw-data',
           exclude: [{ file: "**/node_modules/**" }],
           emitFile: true,
           sourcemap: true,
