caad71932714e83007f97b4481cd12d9b684b05e
Denis Bardadym <denbard@amazon.com>
2024-12-27 11:04:49 +0100

    Reformat

--- plugin/template-types.ts
+++ plugin/template-types.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-export type TemplateType = "sunburst" | "treemap" | "network" | "raw-data" | "list" ;
+export type TemplateType = "sunburst" | "treemap" | "network" | "raw-data" | "list";
 
 const templates: ReadonlyArray<TemplateType> = [
   "sunburst",
